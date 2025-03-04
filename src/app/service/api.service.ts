3a41e3c25f2c3a33c9241a3613403c6569c61662
abdelmageed2062019 <abdelmaged.hamdi1@gmail.com>
2025-03-04 16:54:02 +0200

    update assets

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private apiUrl = 'https://admin.thebegames.com/apis/v1/sports-app';
+  private authToken = "68dc9ccc69df375db9083d58a169d73dbcb5bd07";
 
   constructor(private http: HttpClient) { }
 
@@ -36,7 +37,18 @@ export class ApiService {
     });
   }
 
-  updateMatchScore(matchId: number, score: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/matches/${matchId}`, { score });
+  getMatchDetails(matchId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/matches/${matchId}/`);
+  }
+
+  updateMatch(matchId: number, matchDaata: any): Observable<any> {
+    const url = `${this.apiUrl}${matchId}/`;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Token ${this.authToken}`
+    });
+
+    return this.http.put(url, matchDaata, { headers });
   }
 }
--- src/app/update-match/update-match.component.ts
+++ src/app/update-match/update-match.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../service/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-match',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './update-match.component.html',
   styleUrl: './update-match.component.css'
 })
-export class UpdateMatchComponent {
+export class UpdateMatchComponent implements OnInit {
+  matchId!: number;
+  matchData: any = {};
 
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('matchId');
+      if (id) {
+        this.matchId = +id;
+        this.getMatchDetails(this.matchId);
+      }
+    });
+  }
+
+  getMatchDetails(matchId: number): void {
+    this.apiService.getMatchDetails(matchId).subscribe({
+      next: (data) => {
+        this.matchData = data;
+      },
+      error: (error) => {
+        console.error('Error fetching match details:', error);
+      }
+    });
+  }
 }
